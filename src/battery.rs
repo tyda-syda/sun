1b27df51e7ca24faad377e2e71ec2d4c82c09ee2
tyda-syda <teiron2001@gmail.com>
2025-08-11 21:10:25 +0200

    Refactor battery module
    pause poll on fully charged battery

--- src/battery.rs
+++ src/battery.rs
@@ -5,7 +5,9 @@ use notify_rust::Urgency;
 use std::fs;
 use std::str::FromStr;
 
-const SYS_PATH: &'static str = "/sys/class/power_supply/BAT0";
+const BATTERY_POLL_TIMEOUT: i32 = 15 * 1000; // msec
+const BATTERY_WARN_AT: u8 = 15;
+const SYS_PATH: &'static str = "/sys/class/power_supply/BAT0/uevent";
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum Status {
@@ -22,13 +24,17 @@ struct UeventPowerSupply {
 
 impl UeventPowerSupply {
     pub fn new() -> Result<Self, String> {
-        let uevent_str =
-            &fs::read_to_string(format!("{SYS_PATH}/uevent")).map_err(|e| e.to_string())?;
-        let mut capacity = ev_utils::get_element_val(&uevent_str, "POWER_SUPPLY_CAPACITY")
-            .ok_or("POWER_SUPPLY_CAPACITY missing".to_owned())
-            .map(|cap| u8::from_str(&cap).map_err(|err| err.to_string()));
+        let uevent_str = fs::read_to_string(SYS_PATH).map_err(|e| e.to_string())?;
+        let status = ev_utils::get_element_val(&uevent_str, "POWER_SUPPLY_STATUS")
+            .ok_or("POWER_SUPPLY_STATUS missing".to_owned())?
+            .into();
 
-        if matches!(capacity, Err(_)) {
+        if let Some(capacity) = ev_utils::get_element_val(&uevent_str, "POWER_SUPPLY_CAPACITY") {
+            Ok(Self {
+                status,
+                capacity: u8::from_str(&capacity).map_err(|err| err.to_string())?,
+            })
+        } else {
             let now = ev_utils::get_element_val(&uevent_str, "POWER_SUPPLY_ENERGY_NOW")
                 .ok_or("POWER_SUPPLY_ENERGY_NOW missing".to_owned())
                 .map(|now| f32::from_str(&now))?
@@ -38,15 +44,11 @@ impl UeventPowerSupply {
                 .map(|now| f32::from_str(&now))?
                 .map_err(|err| err.to_string())?;
 
-            capacity = Ok(Ok((now / full * 100.) as u8));
+            Ok(Self {
+                status,
+                capacity: (now / full * 100.) as u8,
+            })
         }
-
-        Ok(Self {
-            status: ev_utils::get_element_val(&uevent_str, "POWER_SUPPLY_STATUS")
-                .ok_or("POWER_SUPPLY_STATUS missing".to_owned())?
-                .into(),
-            capacity: capacity??,
-        })
     }
 }
 
@@ -97,18 +99,20 @@ impl ToString for Status {
 pub fn routine() -> impl crate::Routine {
     || {
         let mut handle = NetlinkHandle::new().unwrap();
-        let mut last_status = UeventPowerSupply::new().unwrap().status;
-        let mut full = false;
         let mut notif = NotifWrapper::new();
+        let mut last_status = UeventPowerSupply::new().unwrap().status;
+        let mut poll_timeout = BATTERY_POLL_TIMEOUT;
+        let mut full = false;
 
         loop {
-            match handle.read_uevent_msec::<UeventPowerSupply, String>(15 * 1000) {
+            match handle.read_uevent_msec::<UeventPowerSupply, String>(poll_timeout) {
                 Ok(ev) => {
                     if ev.status == last_status {
                         continue;
                     }
 
                     full = false;
+                    poll_timeout = BATTERY_POLL_TIMEOUT;
                     last_status = ev.status;
 
                     notif
@@ -141,6 +145,7 @@ pub fn routine() -> impl crate::Routine {
 
                     if !full && uevent.status == Status::Full {
                         full = true;
+                        poll_timeout = -1; // wait for uevent, no need to poll for now
 
                         notif.body("Battery is full");
                         notif.icon += "battery-level-100-charged-symbolic.svg";
@@ -151,7 +156,7 @@ pub fn routine() -> impl crate::Routine {
 
                     let cap = uevent.capacity;
 
-                    if uevent.status == Status::Discharging && cap <= 15 {
+                    if uevent.status == Status::Discharging && cap <= BATTERY_WARN_AT {
                         notif.body(format!("{cap}% left, connect charger").as_str());
                         notif.urgency(Urgency::Critical);
                         notif.icon += "battery-caution-symbolic.svg";
